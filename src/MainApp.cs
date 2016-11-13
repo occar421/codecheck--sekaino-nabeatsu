4f63323a0686563a38b8b2071399dacf091bc333
code-project <dev@code-check.io>
2016-11-13 04:01:24 +0000

    Add new CLI related files

---
+++ src/MainApp.cs
@@ -0,0 +1,11 @@
+using System;
+ 
+public class MainApp
+{
+    static public void Main (string[] args)
+    {
+        foreach (string arg in args) {
+           Console.WriteLine(arg);
+        }
+    }
+}
