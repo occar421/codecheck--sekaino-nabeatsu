8c6ab5b6e5ed81e215e2fcd9b9dc25e88b2b4c79
MasuqaT <occar@hotmail.co.jp>
2016-11-13 15:06:52 +0900

    Implemented program for problem1

--- src/MainApp.cs
+++ src/MainApp.cs
@@ -1,11 +1,28 @@
-using System;
- 
+using System; // no longer needed in .NET Core
+
 public class MainApp
 {
-    static public void Main (string[] args)
+    static public void Main(string[] args)
     {
-        foreach (string arg in args) {
-           Console.WriteLine(arg);
+        foreach (string arg in args)
+        {
+            int num;
+            if (int.TryParse(arg, out num))
+            {
+                var contains3 = arg.Contains("3");
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine(contains3 ? "dumb" : "idiot");
+                }
+                else
+                {
+                    Console.WriteLine(contains3 ? "stupid" : "smart");
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
         }
     }
 }
